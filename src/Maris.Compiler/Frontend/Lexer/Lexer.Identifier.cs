aee0371868f99a7a71954e93081ed9011ea03387
cwahl02 <cwahl02git@gmail.com>
2026-08-05 16:18:19 -0700

    Created Lexer class split into multiple partial classes

---
+++ src/Maris.Compiler/Frontend/Lexer/Lexer.Identifier.cs
@@ -0,0 +1,70 @@
+namespace Maris.Compiler.Lexer;
+
+using Maris.Core.Text;
+
+public sealed partial class Lexer
+{
+    private Token LexIdentifier()
+    {
+        var start = _position;
+        while (!_isAtEnd && IsIdentifierPart(_current))
+        {
+            _advance();
+        }
+
+        var value = _text.AsSpan(start, _position - start);
+        var type = IsKeyword(value);
+        return MakeToken(type, start, _position - start);
+    }
+
+    private static bool IsIdentifierStart(char c) => char.IsLetter(c) || c == '_';
+    private static bool IsIdentifierPart(char c) => char.IsLetterOrDigit(c) || c == '_';
+
+    private static TokenType IsKeyword(ReadOnlySpan<char> value)
+    {
+        return value switch
+        {
+            "if" => TokenType.If,
+            "else" => TokenType.Else,
+            "continue" => TokenType.Continue,
+            "break" => TokenType.Break,
+            "return" => TokenType.Return,
+            "switch" => TokenType.Switch,
+            "case" => TokenType.Case,
+            "default" => TokenType.Default,
+            "defer" => TokenType.Defer,
+            "match" => TokenType.Match,
+
+            "while" => TokenType.While,
+            "for" => TokenType.For,
+            "foreach" => TokenType.Foreach,
+
+            "u8" => TokenType.U8,
+            "u16" => TokenType.U16,
+            "u32" => TokenType.U32,
+            "u64" => TokenType.U64,
+
+            "i8" => TokenType.I8,
+            "i16" => TokenType.I16,
+            "i32" => TokenType.I32,
+            "i64" => TokenType.I64,
+
+            "f32" => TokenType.F32,
+            "f64" => TokenType.F64,
+
+            "void" => TokenType.Void,
+            "bool" => TokenType.Bool,
+            "string" => TokenType.String,
+
+            "alias" => TokenType.Alias,
+            "distinct" => TokenType.Distinct,
+            "enum" => TokenType.Enum,
+            "struct" => TokenType.Struct,
+            "union" => TokenType.Union,
+
+            "array" => TokenType.Array,
+            "slice" => TokenType.Slice,
+            _ => TokenType.Identifier
+        };
+    }
+}
---
+++ src/Maris.Compiler/Frontend/Lexer/Lexer.cs
@@ -0,0 +1,86 @@
+namespace Maris.Compiler.Lexer;
+
+using Maris.Core.Text;
+
+public sealed partial class Lexer
+{
+    private string _text;
+    private readonly TextWindow _textWindow;
+    private int _position => _textWindow.Position;
+    private char _current => _textWindow.Current;
+    private char _peek(int offset) => _textWindow.Peek(offset);
+    private bool _isAtEnd => _textWindow.IsAtEnd;
+    private void _advance() => _textWindow.Advance();
+    private void _advance(int count) => _textWindow.Advance(count);
+
+    public Lexer(string text)
+    {
+        _text = text;
+        _textWindow = new TextWindow(text);
+    }
+
+    public List<Token> Lex()
+    {
+        var tokens = new List<Token>();
+
+        while (!_isAtEnd)
+        {
+            SkipTrivia();
+
+            if (_isAtEnd)
+            {
+                break;
+            }
+            
+            tokens.Add(LexToken());
+        }
+
+        tokens.Add(MakeToken(TokenType.EOF, _position, 0));
+
+        return tokens;
+    }
+
+    private Token LexToken()
+    {
+        if (IsIdentifierStart(_current))
+        {
+            return LexIdentifier();
+            // Add token to the list of tokens
+        }
+
+        return _current switch
+        {
+            '+' => LexPlus(),
+            '-' => LexMinus(),
+            '*' => LexStar(),
+            '/' => LexSlash(),
+            '%' => LexPercent(),
+            '=' => LexEqual(),
+
+            '^' => LexCaret(),
+            '&' => LexAmpersand(),
+            '|' => LexPipe(),
+            '!' => LexExclamation(),
+
+            '<' => LexLess(),
+            '>' => LexGreater(),
+
+            '.' => LexDot(),
+            ':' => LexColon(),
+
+            // TODO: Add support for slice '[]u8' and array '[3]u8' types
+            //'[' => LexLeftBracket(),
+
+            ',' => LexSingle(TokenType.Comma),
+            ';' => LexSingle(TokenType.Semicolon),
+            '(' => LexSingle(TokenType.LeftParen),
+            ')' => LexSingle(TokenType.RightParen),
+            '[' => LexSingle(TokenType.LeftBracket),
+            ']' => LexSingle(TokenType.RightBracket),
+            '{' => LexSingle(TokenType.LeftBrace),
+            '}' => LexSingle(TokenType.RightBrace),
+
+            _ => LexUnknown()
+        };
+    }
+}
