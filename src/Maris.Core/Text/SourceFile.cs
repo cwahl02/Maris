383b75309bdbdefa022f20e9b25a663943c741e7
cwahl02 <cwismgitism@gmail.com>
2026-08-17 12:08:07 -0700

    Introduced a new constructor for taking only a filepath string

--- src/Maris.Core/Text/SourceFile.cs
+++ src/Maris.Core/Text/SourceFile.cs
@@ -7,6 +7,13 @@ public sealed class SourceFile
     public string Text { get; }
     public int Length => Text.Length;
 
+    public SourceFile(string filePath)
+    {
+        FilePath = filePath;
+        Name = System.IO.Path.GetFileName(filePath);
+        Text = System.IO.File.ReadAllText(filePath);
+    }
+
     public SourceFile(string filePath, string text)
     {
         FilePath = filePath;
