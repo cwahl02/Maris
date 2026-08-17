3c49d0c1b020adc3c048b3b8c8ce0d26167b8456
cwahl02 <cwahl02git@gmail.com>
2026-08-17 02:25:10 -0700

    Redone `Compilation` and `CompilationUnit` objects moved under `Maris.Compiler.Compilation`

---
+++ src/Maris.Compiler/Compilation/Compilation.cs
@@ -0,0 +1,17 @@
+namespace Maris.Compiler.Compilation;
+
+public sealed class Compilation
+{
+    public ScopeNode GlobalScope { get; } = new ScopeNode();
+    public List<CompilationUnit> Units { get; } = [];
+
+    public Compilation(IEnumerable<string> FilePaths)
+    {
+        foreach (var filePath in FilePaths)
+        {
+            var source = new SourceFile(filePath);
+            var unit = new CompilationUnit(source);
+            Units.Add(unit);
+        }
+    }
+}
---
+++ src/Maris.Compiler/Compilation/CompilationUnit.cs
@@ -0,0 +1,43 @@
+using Maris.Compiler.Lexing;
+using Maris.Compiler.Parsing.Syntax;
+using Maris.Core.Diagnostics;
+
+namespace Maris.Compiler.Compilation;
+
+public enum CompilationPhase
+{
+    Initial,
+    Lexing,
+    Parsing,
+    Binding
+}
+
+public sealed class CompilationUnit
+{
+    public SourceFile SourceFile { get; }
+    public CompilationPhase Phase { get; } = CompilationPhase.Initial;
+
+    public IReadOnlyList<Token>? Tokens { get; }
+    public SyntaxNode? SyntaxTree { get; }
+    // public BoundNode? BoundTree { get; }
+
+    public DiagnosticBag Diagnostics { get; }
+    public bool HasErrors => Diagnostics.HasErrors;
+    public CompilationUnit(
+        SourceFile sourceFile,
+        IReadOnlyList<Token>? tokens,
+        SyntaxNode? syntaxTree,
+        // BoundNode? boundTree,
+        DiagnosticBag diagnostics
+        )
+    {
+        SourceFile = sourceFile;
+        Tokens = tokens;
+        SyntaxTree = syntaxTree;
+        Diagnostics = diagnostics;
+    }
+
+    public bool HasTokens => Tokens is not null;
+    public bool HasSyntaxTree => SyntaxTree is not null;
+    //public bool HasBoundTree => false; // BoundTree is not implemented yet
+}
