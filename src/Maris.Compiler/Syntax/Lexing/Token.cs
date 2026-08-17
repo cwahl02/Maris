39a3e9651354e9c5740ba7d81ca0bec84049eb76
cwahl02 <cwismgitism@gmail.com>
2026-08-17 12:07:29 -0700

    Added extensions to IReadOnlyList for better rdomain separation when doing unit testing. Separates Assert from domains

--- src/Maris.Compiler/Syntax/Lexing/Token.cs
+++ src/Maris.Compiler/Syntax/Lexing/Token.cs
@@ -10,3 +10,65 @@ public sealed record Token(
     public static readonly Token Eof = new(TokenKind.Eof, TextSpan.Empty);
     public Token(TokenKind kind, int start, int length) : this(kind, new TextSpan(start, length)) { }
 }
+
+public static class TokenListExtensions
+{
+    public static bool Compare(
+        this IReadOnlyList<Token> expected,
+        IReadOnlyList<Token> actual
+    )
+    {
+        if (expected.Count != actual.Count)
+            return false;
+
+        for (int i = 0; i < expected.Count; i++)
+        {
+            if(!expected[i].Equals(actual[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+    public static bool Contains(
+        this IReadOnlyList<Token> actual,
+         params TokenKind[] kinds)
+    {
+        HashSet<TokenKind> set = new();
+
+        foreach (Token token in actual)
+        {
+            set.Add(token.Kind);    
+        }
+
+        foreach (TokenKind kind in kinds)
+        {
+            if (set.Contains(kind))
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool Contains(
+        this IReadOnlyList<Token> actual,
+        string sourceText,
+        params string[] texts
+    )
+    {
+        HashSet<string> set = new();
+
+        foreach (Token token in actual)
+        {
+            set.Add(sourceText.Substring(token.Span.Start, token.Span.Length));    
+        }
+
+        foreach (string text in texts)
+        {
+            if (set.Contains(text))
+                return true;
+        }
+
+        return false;
+    }
+}
