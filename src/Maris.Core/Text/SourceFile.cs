41719094b0c60e28f581f2cc02a69f02e3846178
cwahl02 <cwahl02git@gmail.com>
2026-08-17 02:26:11 -0700

    Add Name property to SourceFile class and initialize it in the constructor

--- src/Maris.Core/Text/SourceFile.cs
+++ src/Maris.Core/Text/SourceFile.cs
@@ -3,11 +3,14 @@ namespace Maris.Core.Text;
 public sealed class SourceFile
 {
     public string FilePath { get; }
+    public string Name { get; }
     public string Text { get; }
+    public int Length => Text.Length;
 
     public SourceFile(string filePath, string text)
     {
         FilePath = filePath;
+        Name = System.IO.Path.GetFileName(filePath);
         Text = text;
     }
 }
