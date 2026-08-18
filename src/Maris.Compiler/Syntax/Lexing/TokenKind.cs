fb6422b37106ea3d9f6af9f4f2155b3946a50d3b
cwahl02 <cwahl02git@gmail.com>
2026-08-17 21:11:07 -0700

    Add additional token kinds for improved lexing support

--- src/Maris.Compiler/Syntax/Lexing/TokenKind.cs
+++ src/Maris.Compiler/Syntax/Lexing/TokenKind.cs
@@ -81,8 +81,62 @@ public enum TokenKind
 
     // Tilde
     Tilde,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+    Comma,
+    Semicolon,
+    Underscore,
 
     // Keywords
+
+    // Control Flow
+    If,
+    Else,
+    While,
+    For,
+    Return,
+    Defer,
+    Continue,
+    Break,
+    Switch,
+    Case,
+    Default,
+    Match,
+
+    // Primitive Types
+    U8,
+    U16,
+    U32,
+    U64,
+    I8,
+    I16,
+    I32,
+    I64,
+    F32,
+    F64,
+    Void,
+    Bool,
+    String,
+
+    // User Defined Types
+    Alias,
+    Distinct,
+    Enum,
+    Struct,
+    Union,
+
+    // Boolean Literals
+    True,
+    False,
+
+    // Boolean Operators
+    And,
+    Or,
+
     Import,
     Module,
     As
