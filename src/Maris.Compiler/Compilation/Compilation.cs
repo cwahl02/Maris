b67b0a31f637cb46f0160ef67821f809bb201608
cwahl02 <cwahl02git@gmail.com>
2026-08-24 01:29:38 -0700

    Add CompilationPhase enum and initialize Phase in Compilation class

--- src/Maris.Compiler/Compilation/Compilation.cs
+++ src/Maris.Compiler/Compilation/Compilation.cs
@@ -2,10 +2,20 @@ using Maris.Core.Text;
 
 namespace Maris.Compiler.Compilation;
 
+public enum CompilationPhase
+{
+    Initial,
+    Lexing,
+    Parsing,
+    Binding
+}
+
 public sealed class Compilation
 {
+    public CompilationPhase Phase { get; } = CompilationPhase.Initial;
     public ScopeNode GlobalScope { get; } = new ScopeNode();
-    public List<CompilationUnit> Units { get; } = [];
+    public List<SourceArtifacts> Units { get; } = [];
+    public SemanticArtifacts? SemanticArtifacts { get; private set; }
 
     public Compilation(IEnumerable<string> FilePaths)
     {
